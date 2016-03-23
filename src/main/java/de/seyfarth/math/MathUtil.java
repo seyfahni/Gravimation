c5fe4439c6c249160b4f5f39bf441ebb47562af9
Niklas Seyfarth <niklas@seyfarth.de>
2016-03-23 22:27:05 +0100

    Added nth root calcuation for BigDecimals.

--- src/main/java/de/seyfarth/math/MathUtil.java
+++ src/main/java/de/seyfarth/math/MathUtil.java
@@ -19,6 +19,22 @@ public class MathUtil {
         return x1;
     }
     
+    public static BigDecimal root(BigDecimal A, final int N, final int SCALE) {
+        BigDecimal x0 = BigDecimal.ZERO;
+        BigDecimal x1 = new BigDecimal(Math.pow(A.doubleValue(), 1.0/N));
+        BigDecimal n = new BigDecimal(N);
+        BigDecimal nSubOne = n.subtract(BigDecimal.ONE);
+        int NSubOne = N-1;
+        while (!x0.equals(x1)) {
+            x0 = x1;
+            x1 = A.divide(x0.pow(NSubOne), SCALE, BigDecimal.ROUND_HALF_UP);
+            x1 = x1.add(x0.multiply(nSubOne));
+            x1 = x1.divide(n, SCALE, BigDecimal.ROUND_HALF_UP);
+
+        }
+        return x1;
+    }
+    
     public static Vector movingVector(Point from, Point to) {
         Vector fromVector = from.positionVector();
         Vector toVector = to.positionVector();
